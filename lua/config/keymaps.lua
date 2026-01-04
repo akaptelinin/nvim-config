cc0f609fe22dcad2d9c6ec6eb59a420a586e7d23
akaptelinin <akaptelinin@pyrus.com>
2026-01-04 10:40:42 +0300

    remap flash to <leader>ss, restore original s

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,4 +2,13 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+-- HACK: Escape and Alt+[ get hijacked by some unholy nvim/lazyvim/snacks cache demon
+-- that survives config resets, cache wipes, and even full system reboots.
+-- The only way to restore default terminal behavior is to explicitly remap them to themselves.
+-- I hate this editor with every fiber of my being.
+vim.keymap.set('t', '<Esc>', '<Esc>', { noremap = true })
+vim.keymap.set('t', '<A-[>', '<A-[>', { noremap = true })
+
 vim.keymap.set('t', '<C-[>', '<C-\\><C-n>', { noremap = true })
+
+vim.keymap.set({ "n", "x" }, "s", "s", { noremap = true, desc = "Substitute char" })
---
+++ lua/plugins/flash.lua
@@ -0,0 +1,22 @@
+return {
+  "folke/flash.nvim",
+  keys = {
+    { "s", mode = { "n", "x", "o" }, false },
+    {
+      "<leader>ss",
+      mode = { "n", "x", "o" },
+      function()
+        require("flash").jump()
+      end,
+      desc = "Flash",
+    },
+    {
+      "<leader>sS",
+      mode = { "n", "x", "o" },
+      function()
+        require("flash").treesitter()
+      end,
+      desc = "Flash Treesitter",
+    },
+  },
+}
