e36af1e3ddbfa33f3657b96da272f14b178d19af
akaptelinin <akaptelinin@pyrus.com>
2026-01-06 04:04:00 +0300

    tsgo lsp, disable marks

---
+++ lua/plugins/lspconfig.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        -- tsgo: новый TS LSP на Go от Microsoft (быстрее в 10x)
+        -- npm install -g @typescript/native-preview
+        tsgo = {},
+
+        -- отключаем старые TS серверы:
+        vtsls = { enabled = false },  -- закомментить если tsgo глючит
+        ts_ls = { enabled = false },     -- обёртка над tsserver
+        tsserver = { enabled = false },  -- оригинальный от MS
+      },
+    },
+  },
+}
