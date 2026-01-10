54694b7f68be2c6cd552418bd4b404d6748ae112
akaptelinin <akaptelinin@pyrus.com>
2026-01-10 07:55:15 +0300

    add go-unfucked plugin

---
+++ lua/plugins/go-unfucked.lua
@@ -0,0 +1,22 @@
+return {
+  "akaptelinin/go-unfucked.nvim",
+  ft = "go",
+  opts = {
+    import_hints = {
+      enabled = true,
+    },
+    receiver_highlight = {
+      color = "#ff9900",
+    },
+    error_dim = {
+      enabled = true,
+      dim_simple_return = true,
+      dim_wrapped_return = true,
+      dim_color = "#666666",
+      dim_blend = 60,
+    },
+    shortnames = {
+      enabled = true,
+    },
+  },
+}
