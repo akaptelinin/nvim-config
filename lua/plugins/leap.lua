59688656dcb20f47de1e97eff94defc7857d3962
akaptelinin <akaptelinin@pyrus.com>
2026-02-25 22:55:09 +0300

    leap.nvim: the repository has been moved to Codeberg.
      In your package manager, change the url to `https://codeberg.org/andyg/leap.nvim`.
    
      In lazy.nvim:
        before:
        {
          "ggandor/leap.nvim",
          -- config...
        }
        after:
        {
          url = "https://codeberg.org/andyg/leap.nvim",
          -- config...
        }/////  пофикси коммит и пуш

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -2,7 +2,7 @@
 -- flash.nvim handles f/F/t/T/;/, (see flash.lua)
 return {
   {
-    "ggandor/leap.nvim",
+    url = "https://codeberg.org/andyg/leap.nvim",
     config = function()
       vim.keymap.set({"n", "x", "o"}, "s", function() require("leap").leap({ inclusive = true }) end, { desc = "Leap forward" })
       vim.keymap.set({"n", "x", "o"}, "S", function() require("leap").leap({ backward = true }) end, { desc = "Leap backward" })
